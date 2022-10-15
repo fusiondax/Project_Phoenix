e4706bb8a37c635865e1399b35f0a304b3ffcf23
fusiondax <superdax1@hotmail.com>
2022-10-15 13:52:39 -0400

    Reintroducing graphical assets and gamemaps into the git repo
    they were ignored before and I'm not sure why...

---
+++ Phoenix_libgdx/core/assets/graphics/tilesets/Default Blueprint Tileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Default Blueprint Tileset" tilewidth="64" tileheight="64" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="64" height="64" source="../blueprint.png"/>
+ </tile>
+</tileset>
